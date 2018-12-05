417e6d52e557a10230792fb640c08d064b34032b
lhossackucalgary <lhossack@ucalgary.ca>
2018-12-05 14:31:42 -0700

    move studentviews to extern file

---
+++ js/studentViews.js
@@ -0,0 +1,54 @@
+function viewStudents(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("query-results").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", "php/viewStudents.php", true);
+    xhttp.send();
+}
+
+//Note: This is very not secure..
+function viewClasses(student_id) {
+    var formData = new FormData();
+    formData.append("student_id", student_id);
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("query-results").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("POST", "php/viewStudentClasses.php", true);
+    xhttp.send(formData);
+}
+
+function dropCourse(student_id, class_id) {
+    var formData = new FormData();
+    formData.append("student_id", student_id);
+    formData.append("class_id", class_id);
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("query-results").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("POST", "php/dropClass.php", true);
+    xhttp.send(formData);
+}
+
+function viewSessions(student_id) {
+    var formData = new FormData();
+    formData.append("student_id", student_id);
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("query-results").innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("POST", "php/viewStudentSessions.php", true);
+    xhttp.send(formData);
+}
--- logged_in_zone.php
+++ logged_in_zone.php
@@ -37,65 +37,9 @@ if(!array_key_exists('username', $_SESSION)){
         </form>
 
         <div id="query-results">
-
         </div>
 
-        <script type="text/javascript">
-            function viewStudents(){
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        document.getElementById("query-results").innerHTML = this.responseText;
-                    }
-                };
-                xhttp.open("GET", "php/viewStudents.php", true);
-                xhttp.send();
-            }
-
-            //Note: This is very not secure..
-            function viewClasses(student_id) {
-                var formData = new FormData();
-                formData.append("student_id", student_id);
-
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        document.getElementById("query-results").innerHTML = this.responseText;
-                    }
-                };
-                xhttp.open("POST", "php/viewStudentClasses.php", true);
-                xhttp.send(formData);
-            }
-
-            function dropCourse(student_id, class_id) {
-                var formData = new FormData();
-                formData.append("student_id", student_id);
-                formData.append("class_id", class_id);
-
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        document.getElementById("query-results").innerHTML = this.responseText;
-                    }
-                };
-                xhttp.open("POST", "php/dropClass.php", true);
-                xhttp.send(formData);
-            }
-
-            function viewSessions(student_id) {
-                var formData = new FormData();
-                formData.append("student_id", student_id);
-
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        document.getElementById("query-results").innerHTML = this.responseText;
-                    }
-                };
-                xhttp.open("POST", "php/viewStudentSessions.php", true);
-                xhttp.send(formData);
-            }
-        </script>
+        <script type="text/javascript" src="js/studentViews.js"></script>
         <script type="text/javascript" src="js/buttonLoaders.js"></script>
         <script type="text/javascript" src="js/formLoaders.js"></script>
         <script type="text/javascript" src="js/loadDoc.js"></script>
