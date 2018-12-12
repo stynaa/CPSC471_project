96ed3ebdfbd0652ca4ed4b1a3af84ec42d79b23c
lhossackucalgary <lhossack@ucalgary.ca>
2018-12-11 22:58:48 -0700

    Add no results found to searchtutors

--- php/listTutorSearchResults.php
+++ php/listTutorSearchResults.php
@@ -22,20 +22,23 @@ $lname = '%' . $lname . '%';
 $uname = '%' . $uname . '%';
 $city = '%' . $city . '%';
 
-
 if ($stmt->execute()) {
     $result = $stmt->get_result();
     echo '<h3 class="title">Search results (Tutors):</h3>';
-    echo "<table>";
-    echo '<th>Username</th><th>First Name</th><th>Last Name</th><th>Education</th><th>Bio</th><th>City</th><th>Select</th>';
-    while($x = $result->fetch_assoc()) {
-        echo '<tr>' . '<td>'.$x["username"].'</td><td>'.$x["first_name"].
-            '</td><td>'.$x["last_name"].'</td><td>'.$x["education"].'</td><td>'.
-            '</td><td>'.$x["bio"].'</td><td>'.$x["city"].'</td><td>'.
-            '<button type="button" onclick="parentViewTutorProfile(\'' . $x["username"] . '\')">View Tutor</button>'.
-            '</tr>';
+    if ($result->num_rows === 0) {
+        echo "<p>No results found</p>";
+    } else {
+        echo "<table>";
+        echo '<th>Username</th><th>First Name</th><th>Last Name</th><th>Education</th><th>Bio</th><th>City</th><th>Select</th>';
+        while($x = $result->fetch_assoc()) {
+            echo '<tr>' . '<td>'.$x["username"].'</td><td>'.$x["first_name"].
+                '</td><td>'.$x["last_name"].'</td><td>'.$x["education"].'</td><td>'.
+                '</td><td>'.$x["bio"].'</td><td>'.$x["city"].'</td><td>'.
+                '<button type="button" onclick="parentViewTutorProfile(\'' . $x["username"] . '\')">View Tutor</button>'.
+                '</tr>';
+        }
+        echo "</table>";
     }
-    echo "</table>";
 } else {
     echo "e";
 }
