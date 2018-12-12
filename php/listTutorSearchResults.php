fffe6ae2cee73a8faa46142f2b7a86c3e60e2da4
lhossackucalgary <lhossack@ucalgary.ca>
2018-12-11 20:58:10 -0700

    search repaired

--- php/listTutorSearchResults.php
+++ php/listTutorSearchResults.php
@@ -13,15 +13,14 @@ $lname = $_POST["last_name"];
 $uname = $_POST["username"];
 $city = $_POST["city"];
 
-$stmt = $conn->prepare("SELECT * FROM Tutor AS t LEFT JOIN User AS u ON t.username = u.username WHERE u.first_name LIKE ? AND u.last_name LIKE ? AND u.username LIKE ? AND t.city LIKE ?");
 
-echo var_dump($stmt);
+$stmt = $conn->prepare("SELECT * FROM Tutor AS t LEFT JOIN User AS u ON t.username = u.username WHERE u.first_name LIKE ? AND u.last_name LIKE ? AND u.username LIKE ? AND t.city LIKE ?");
 $stmt->bind_param("ssss", $fname, $lname, $uname, $city);
 
-$fname = "%" + $fname + "%";
-$lname = "%" + $lname + "%";
-$uname = "%" + $uname + "%";
-$city = "%" + $city + "%";
+$fname = '%' . $fname . '%';
+$lname = '%' . $lname . '%';
+$uname = '%' . $uname . '%';
+$city = '%' . $city . '%';
 
 
 if ($stmt->execute()) {
